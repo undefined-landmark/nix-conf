34e8721ed2366bb2456b250bc35ae48fdbaa3f3d
bas <bas@noemail.invalid>
2024-11-19 21:27:47 +0100

    Add options to nixd

--- hosts/nixvm/home.nix
+++ hosts/nixvm/home.nix
@@ -43,6 +43,7 @@
             settings = {
               nixpkgs.expr = "import nixpkgs { }";
               formatting.command = ["alejandra"];
+              options.nixos.expr = "(builtins.getFlake \"/home/bas/nix-conf/flake.nix\").nixosConfigurations.nixvm.options";
             };
           };
         };
