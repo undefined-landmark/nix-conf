1df0a84fb0c2111ff8cd3c0ff37c60b47c71f060
bas <bas@noemail.invalid>
2024-11-26 16:05:00 +0100

    Add cmp to nixvim

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -4,6 +4,7 @@
     defaultEditor = true;
     vimdiffAlias = true;
     plugins = {
+      fugitive.enable = true;
       lsp = {
         enable = true;
         keymaps.lspBuf = {
@@ -13,14 +14,23 @@
           nixd = {
             enable = true;
             settings = {
-              nixpkgs.expr = "import nixpkgs { }";
+              nixpkgs.expr = "import <nixpkgs> { }";
               formatting.command = ["alejandra"];
-              options.nixos.expr = "(builtins.getFlake \"/home/bas/nix-conf/flake.nix\").nixosConfigurations.nixvm.options";
+              options.nixos.expr = "(builtins.getFlake \"/home/bas/nix-conf\").nixosConfigurations.lightbox.options";
+              options.home_manager.expr = "(import <home-manager/modules> { configuration = /home/bas/nix-conf/hosts/lightbox/home.nix; pkgs = import <nixpkgs> {}; }).options";
             };
           };
         };
       };
-      fugitive.enable = true;
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings.sources = [
+          {name = "nvim_lsp";}
+          {name = "path";}
+          {name = "buffer";}
+        ];
+      };
     };
   };
 }
