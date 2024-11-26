e59712396442ff159d35d476989942a0c80a3a14
bas <bas@noemail.invalid>
2024-11-26 11:25:23 +0100

    Make lf cd after exit

--- home/general.nix
+++ home/general.nix
@@ -1,9 +1,14 @@
-{
+{pkgs, ...}: {
   programs.home-manager.enable = true;
   home.stateVersion = "24.05";
   home.username = "bas";
   home.homeDirectory = "/home/bas";
 
   nixpkgs.config.allowUnfree = true;
-  programs.bash.enable = true;
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = "source ${pkgs.lf.src}/etc/lfcd.sh";
+    shellAliases = {lf = "lfcd";};
+  };
 }
