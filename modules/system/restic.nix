ed9e33de34436b383a808072e086d684436b1c9c
bas <bas@noemail.invalid>
2025-01-05 11:17:40 +0100

    Add restic repofile and passfile because the module expects them

--- modules/system/restic.nix
+++ modules/system/restic.nix
@@ -17,6 +17,8 @@ in {
   config = lib.mkIf cfg.enable {
     custom-modules.sops.enable = true;
     sops.secrets.restic-east_env = {};
+    sops.secrets.restic-east_repo = {};
+    sops.secrets.restic-east_pass = {};
 
     custom-modules.private-vars.enable = true;
 
@@ -24,6 +26,8 @@ in {
       east = {
         createWrapper = true;
         inhibitsSleep = true;
+        repositoryFile = config.sops.secrets.restic-east_repo.path;
+        passwordFile = config.sops.secrets.restic-east_pass.path;
         environmentFile = config.sops.secrets.restic-east_env.path;
 
         paths = config.my-secrets.private.vars.restic-east_paths;
