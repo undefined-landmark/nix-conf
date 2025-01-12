1fbcd714ab04d215d5bd867590f50f21e3b34d88
bas <bas@noemail.invalid>
2025-01-12 10:59:20 +0100

    Add fzf-lua and add cmp keymaps

--- modules/home/pkgs-cli/nixvim.nix
+++ modules/home/pkgs-cli/nixvim.nix
@@ -54,11 +54,24 @@ in {
         cmp = {
           enable = true;
           autoEnableSources = true;
-          settings.sources = [
-            {name = "nvim_lsp";}
-            {name = "path";}
-            {name = "buffer";}
-          ];
+          settings = {
+            sources = [
+              {name = "nvim_lsp";}
+              {name = "path";}
+              {name = "buffer";}
+            ];
+            mapping = {
+              "<C-Space>" = "cmp.mapping.complete()";
+              "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            };
+          };
+        };
+        fzf-lua = {
+          enable = true;
+          keymaps = {
+            "<C-p>" = "oldfiles";
+          };
         };
       };
     };
