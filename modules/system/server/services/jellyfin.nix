fc639bc4faee800ac2c491af786ac0beb71f1679
bas <bas@noemail.invalid>
2025-02-09 18:24:01 +0100

    Add reverse proxy to jellyfin

--- modules/system/server/services/jellyfin.nix
+++ modules/system/server/services/jellyfin.nix
@@ -4,10 +4,29 @@
   ...
 }: let
   cfg = config.custom-modules.server;
+  baseDomain = config.my-secrets.private.vars.domain;
 in {
+  imports = [../../private-vars.nix];
+
   config = lib.mkIf cfg.enable {
+    custom-modules.private-vars.enable = true;
+
     services.jellyfin = {
       enable = true;
+    };
+
+    services.traefik = {
+      dynamicConfigOptions = {
+        http = {
+          routers = {
+            jellyfin.service = "jellyfin";
+            jellyfin.rule = "Host(`jellyfin.${baseDomain}`)";
+          };
+          services = {
+            jellyfin.loadBalancer.servers = [{url = "http://localhost:8096";}];
+          };
+        };
+      };
     };
   };
 }
