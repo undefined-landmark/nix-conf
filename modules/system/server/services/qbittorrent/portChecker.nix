e02651fa32ff555190934d84f4b4e64f121d5cf8
bas <bas@noemail.invalid>
2025-02-16 15:15:44 +0100

    Put port checker service in separate module

---
+++ modules/system/server/services/qbittorrent/portChecker.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  cfg = config.custom-modules.server;
+  portScript = builtins.readFile ./port-checker.sh;
+  creds_path = config.sops.secrets.qbittorrent_post_creds.path;
+  portApp = pkgs.writeShellApplication {
+    name = "qbit-natpmp-port";
+    runtimeInputs = [pkgs.curl pkgs.libnatpmp];
+    text = builtins.replaceStrings ["./creds_path"] [creds_path] portScript;
+  };
+in {
+  imports = [../../../sops.nix];
+
+  config = lib.mkIf cfg.enable {
+    custom-modules.sops.enable = true;
+
+    sops.secrets.qbittorrent_post_creds = {
+      mode = "0440";
+      owner = config.services.qbittorrent.user;
+      group = config.services.qbittorrent.group;
+    };
+
+    systemd.services.qbitPortCheck = {
+      description = "Checks NAT-PMP port and sets the port for qBittorrent";
+      after = [
+        "network.target"
+        "qbittorrent.service"
+        "wg-quick-protonfw.service"
+      ];
+      wantedBy = ["multi-user.target"];
+
+      serviceConfig = {
+        Type = "simple";
+        User = config.services.qbittorrent.user;
+        Group = config.services.qbittorrent.group;
+        DynamicUser = true;
+        ExecStart = lib.getExe portApp;
+        Restart = "on-failure";
+      };
+    };
+  };
+}
