8019c37dad01deacde4a505e1b232261ebe29635
bas <github.plated100@passmail.net>
2025-03-14 19:21:44 +0100

    Allow trim ecobox

--- hosts/ecobox/extra-hardware-configuration.nix
+++ hosts/ecobox/extra-hardware-configuration.nix
@@ -2,6 +2,7 @@
   boot.initrd.luks.devices = {
     "cryptzbig1".device = "/dev/disk/by-uuid/bfca26e6-bfa1-4f07-b694-80c135c447ed";
     "cryptzbig2".device = "/dev/disk/by-uuid/f079793a-a450-4f98-815f-750d76d91669";
+    "cryptroot".allowDiscards = true;
   };
   boot.zfs.extraPools = ["zbig"];
 }
