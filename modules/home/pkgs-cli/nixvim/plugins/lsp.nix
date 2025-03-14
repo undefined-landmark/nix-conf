efd46587c1710bdba98c7bbb8da64b25f825b2aa
bas <github.plated100@passmail.net>
2025-03-14 19:58:00 +0100

    Remove nixd options because I couldn't get the current sysname

--- modules/home/pkgs-cli/nixvim/plugins/lsp.nix
+++ modules/home/pkgs-cli/nixvim/plugins/lsp.nix
@@ -19,8 +19,6 @@ in {
               settings = {
                 nixpkgs.expr = "import <nixpkgs> { }";
                 formatting.command = ["alejandra"];
-                #                options.nixos.expr = "(builtins.getFlake \"/home/bas/git/nix-conf\").nixosConfigurations.lightbox.options";
-                #                options.home_manager.expr = "(import <home-manager/modules> { configuration = /home/bas/git/nix-conf/hosts/lightbox/home.nix; pkgs = import <nixpkgs> {}; }).options";
               };
             };
             bashls.enable = true;
