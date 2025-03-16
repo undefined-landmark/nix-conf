8569d4552050545767386bcc620e4c941c345552
bas <github.plated100@passmail.net>
2025-03-16 15:30:10 +0100

    Combine recyclarr with rest of arr's

--- modules/system/server/services/arr/default.nix
+++ modules/system/server/services/arr/default.nix
@@ -1,13 +1,20 @@
 {
   lib,
   config,
+  inputs,
   ...
 }: let
   cfg = config.custom-modules.server;
 in {
-  imports = [./recyclarr.nix];
+  imports = [
+    "${inputs.nixpkgs-unstable}/nixos/modules/services/misc/recyclarr.nix"
+  ];
 
   config = lib.mkIf cfg.enable {
+    sops.secrets.radarr-api_key = {};
+    sops.secrets.sonarr-api_key = {};
+
+
     services.prowlarr.enable = true;
 
     services.sonarr = {
@@ -34,5 +41,18 @@ in {
         port = "7878";
       }
     ];
+
+    systemd.services.recyclarr.serviceConfig.LoadCredential = [
+      "radarr-api_key:${config.sops.secrets.radarr-api_key.path}"
+      "sonarr-api_key:${config.sops.secrets.sonarr-api_key.path}"
+    ];
+
+    services.recyclarr = {
+      enable = true;
+      configuration = {
+        radarr = config.my-secrets.private.vars.radarrConfig;
+        sonarr = config.my-secrets.private.vars.sonarrConfig;
+      };
+    };
   };
 }
