91bf1d053502c82d2fcd182652e35a06201fafdf
bas <github.plated100@passmail.net>
2025-04-30 15:21:51 +0200

    Change to personal qbit fork

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    qbit.url = "github:fsnkty/nixpkgs/init-nixos-qbittorrent";
+    qbit.url = "github:undefined-landmark/nixpkgs/default-serverConfig";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
