34084caa1cbe30878c4ba0372718166f8c5db18d
bas <github.plated100@passmail.net>
2025-05-04 17:04:57 +0200

    Add username to hm options

--- hosts/ecobox/home.nix
+++ hosts/ecobox/home.nix
@@ -1,13 +1,9 @@
-{
-  config,
-  pkgs,
-  inputs,
-  ...
-}: {
+{...}: {
   imports = [../../modules/home];
 
   custom-home-modules = {
     enable = true;
+    user = "bas";
     general.enable = true;
     pkgs-cli.enable = true;
   };
