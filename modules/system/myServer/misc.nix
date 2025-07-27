1e580a2e68fb5e6c7fbe6ed76731b3a25a21f96c
bas <github.plated100@passmail.net>
2025-07-27 17:59:49 +0200

    Add nix trusted user to myServer

--- modules/system/myServer/misc.nix
+++ modules/system/myServer/misc.nix
@@ -4,11 +4,15 @@
   ...
 }: let
   cfg = config.myServer;
+  username = config.users.users.bas.name;
 in {
   config = lib.mkIf cfg.enable {
     powerManagement.powertop.enable = true;
 
     users.groups."${cfg.mediagroup}" = {};
     users.users.bas.extraGroups = ["${cfg.mediagroup}"];
+
+    # So that --target-host can function
+    nix.settings.trusted-users = [username];
   };
 }
