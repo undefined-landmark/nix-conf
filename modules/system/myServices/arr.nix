98039e38088a00351a2fed13a4bc4ce2b0843f67
bas <github.plated100@passmail.net>
2025-09-06 13:28:49 +0200

    myServices.arr: DRY'er

--- modules/system/myServices/arr.nix
+++ modules/system/myServices/arr.nix
@@ -12,44 +12,37 @@ in {
     sops.secrets.radarr-api_key = {};
     sops.secrets.sonarr-api_key = {};
 
-    services.prowlarr.enable = true;
-
-    services.sonarr = {
-      enable = true;
-      group = mediagroup;
-    };
-
-    services.radarr = {
-      enable = true;
-      group = mediagroup;
-    };
-
-    myServices.traefik.params = [
-      {
-        subdomain = "prowlarr";
-        port = "9696";
-      }
-      {
-        subdomain = "sonarr";
-        port = "8989";
-      }
-      {
-        subdomain = "radarr";
-        port = "7878";
-      }
-    ];
-
     systemd.services.recyclarr.serviceConfig.LoadCredential = [
       "radarr-api_key:${config.sops.secrets.radarr-api_key.path}"
       "sonarr-api_key:${config.sops.secrets.sonarr-api_key.path}"
     ];
 
-    services.recyclarr = {
-      enable = true;
-      configuration = {
-        radarr = config.my-secrets.private.vars.radarrConfig;
-        sonarr = config.my-secrets.private.vars.sonarrConfig;
+    services = {
+      prowlarr.enable = true;
+      sonarr = {
+        enable = true;
+        group = mediagroup;
+      };
+      radarr = {
+        enable = true;
+        group = mediagroup;
+      };
+      recyclarr = {
+        enable = true;
+        configuration = {
+          radarr = config.my-secrets.private.vars.radarrConfig;
+          sonarr = config.my-secrets.private.vars.sonarrConfig;
+        };
       };
     };
+
+    myServices.traefik.params =
+      builtins.map (
+        name: {
+          subdomain = "${name}";
+          port = toString config.services."${name}".settings.server.port;
+        }
+      )
+      ["prowlarr" "sonarr" "radarr"];
   };
 }
