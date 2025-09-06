8a8d4c694e0ed6b3ef5f94c71773dfcae47132de
bas <github.plated100@passmail.net>
2025-09-06 11:17:36 +0200

    myServer: merge misc.nix into default.nix

--- modules/system/myServer/default.nix
+++ modules/system/myServer/default.nix
@@ -1,6 +1,12 @@
-{lib, ...}: {
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.myServer;
+  username = config.users.users.bas.name;
+in {
   imports = [
-    ./misc.nix
     ./postgresqlBackup.nix
     ./remote-unlock.nix
     ./ssh.nix
@@ -8,5 +14,12 @@
 
   options.myServer = {
     enable = lib.mkEnableOption "Server setup";
+  };
+
+  config = lib.mkIf cfg.enable {
+    powerManagement.powertop.enable = true;
+
+    # So that --target-host can function
+    nix.settings.trusted-users = [username];
   };
 }
