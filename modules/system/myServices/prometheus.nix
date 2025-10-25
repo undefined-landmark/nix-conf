e6577106156765b5ffa00afe64acd82cbfbec027
bas <github.plated100@passmail.net>
2025-10-25 18:06:11 +0200

    prometheus: add zfs exporter

--- modules/system/myServices/prometheus.nix
+++ modules/system/myServices/prometheus.nix
@@ -2,20 +2,40 @@
   lib,
   config,
   ...
-}: let
+}:
+let
   cfg = config.myServices.prometheus;
   nodePort = config.services.prometheus.exporters.node.port;
-in {
+  zfsPort = config.services.prometheus.exporters.zfs.port;
+in
+{
   options.myServices.prometheus.enable = lib.mkEnableOption "Setup prometheus";
 
   config = lib.mkIf cfg.enable {
     services.prometheus = {
       enable = true;
 
-      exporters.node = {
-        enable = true;
-        extraFlags = ["--collector.disable-defaults"];
-        enabledCollectors = ["systemd" "cpu" "meminfo" "hwmon" "filesystem"];
+      exporters = {
+        node = {
+          enable = true;
+          extraFlags = [ "--collector.disable-defaults" ];
+          enabledCollectors = [
+            "systemd"
+            "cpu"
+            "meminfo"
+            "hwmon"
+            "filesystem"
+          ];
+        };
+        zfs = {
+          enable = true;
+          extraFlags = [
+            "--no-collector.dataset-filesystem"
+            "--no-collector.dataset-volume"
+            "--properties.pool=\"health\""
+            "--web.disable-exporter-metrics"
+          ];
+        };
       };
 
       scrapeConfigs = [
@@ -23,7 +43,15 @@ in {
           job_name = "node_exporter";
           static_configs = [
             {
-              targets = ["localhost:${toString nodePort}"];
+              targets = [ "localhost:${toString nodePort}" ];
+            }
+          ];
+        }
+        {
+          job_name = "zfs_exporter";
+          static_configs = [
+            {
+              targets = [ "localhost:${toString zfsPort}" ];
             }
           ];
         }
