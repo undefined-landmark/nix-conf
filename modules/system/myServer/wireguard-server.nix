bd0a4c9c29f4be23b4c878e56a4d15a1b871a351
bas <github.plated100@passmail.net>
2025-12-23 11:37:09 +0100

    myServer: enable wireguard

--- modules/system/myServer/wireguard-server.nix
+++ modules/system/myServer/wireguard-server.nix
@@ -33,34 +33,37 @@ in
 
       firewall.allowedUDPPorts = [ wgPort ];
 
-      wireguard.interfaces.wg0 = {
-        # Determines the IP address and subnet of the server's end of the tunnel interface.
-        ips = [ "${ipBase}.1/24" ];
-        listenPort = 51820;
+      wireguard = {
+        enable = true;
+        interfaces.wg0 = {
+          # Determines the IP address and subnet of the server's end of the tunnel interface.
+          ips = [ "${ipBase}.1/24" ];
+          listenPort = 51820;
 
-        postSetup = ''
-          ${forwardLines "-A"}
-          ${postRouting "-A"}
-        '';
-        postShutdown = ''
-          ${forwardLines "-D"}
-          ${postRouting "-D"}
-        '';
+          postSetup = ''
+            ${forwardLines "-A"}
+            ${postRouting "-A"}
+          '';
+          postShutdown = ''
+            ${forwardLines "-D"}
+            ${postRouting "-D"}
+          '';
 
-        privateKeyFile = config.sops.secrets.ecobox_wg-priv.path;
+          privateKeyFile = config.sops.secrets.ecobox_wg-priv.path;
 
-        peers = [
-          {
-            name = "phone";
-            publicKey = "PC3HyEgaifOjnJ0Y30zx32cRpb6HHNE227dM8QPrJlA=";
-            allowedIPs = [ "${ipBase}.2/32" ];
-          }
-          {
-            name = "laptop";
-            publicKey = "npTrLwAIJZ3m4XqdmQpP/KIi0C6urjBQHoCuA1vOOTc=";
-            allowedIPs = [ "${ipBase}.3/32" ];
-          }
-        ];
+          peers = [
+            {
+              name = "phone";
+              publicKey = "PC3HyEgaifOjnJ0Y30zx32cRpb6HHNE227dM8QPrJlA=";
+              allowedIPs = [ "${ipBase}.2/32" ];
+            }
+            {
+              name = "laptop";
+              publicKey = "npTrLwAIJZ3m4XqdmQpP/KIi0C6urjBQHoCuA1vOOTc=";
+              allowedIPs = [ "${ipBase}.3/32" ];
+            }
+          ];
+        };
       };
     };
   };
