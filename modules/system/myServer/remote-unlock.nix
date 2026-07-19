6fa2c1dc4c48717bf52d35b683d142d81ea6beca
bas <github.plated100@passmail.net>
2026-07-19 12:17:15 +0200

    remote-unlock: move to systemd-networkd

--- modules/system/myServer/remote-unlock.nix
+++ modules/system/myServer/remote-unlock.nix
@@ -10,24 +10,30 @@ in
   options.myServer.remote-unlock.enable = lib.mkEnableOption "Setup remote-unlock";
 
   config = lib.mkIf cfg.enable {
-    boot.initrd = {
-      availableKernelModules = [ "e1000e" ];
-      network = {
-        enable = true;
-        udhcpc.enable = true;
-        flushBeforeStage2 = true;
-        ssh = {
+    boot = {
+      kernelParams = [ "rd.systemd.debug_shell=1" ];
+      initrd = {
+        availableKernelModules = [ "e1000e" ];
+        systemd.network = {
           enable = true;
-          port = 22;
-          authorizedKeys = [
-            config.my-secrets.private.vars.yubikey1_pub
-            config.my-secrets.private.vars.yubikey2_pub
-          ];
-          hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+          networks."10-eno1" = {
+            matchConfig.Name = "eno1";
+            networkConfig.DHCP = "ipv4";
+            linkConfig.RequiredForOnline = "routable";
+          };
         };
-        postCommands = ''
-          echo 'cryptsetup-askpass || echo "Unlock was succesful; exiting SSH session" && exit 1' >> /root/.profile
-        '';
+        network = {
+          enable = true;
+          ssh = {
+            enable = true;
+            port = 22;
+            authorizedKeys = [
+              ''command="systemctl default" ${config.my-secrets.private.vars.yubikey1_pub}''
+              ''command="systemctl default" ${config.my-secrets.private.vars.yubikey2_pub}''
+            ];
+            hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+          };
+        };
       };
     };
   };
