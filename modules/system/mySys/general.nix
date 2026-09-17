6ad22050ff9fd3b4f4222502d725bf0efb7c39a5
bas <github.plated100@passmail.net>
2026-09-17 16:25:22 +0200

    nh: keep 5 on clean

--- modules/system/mySys/general.nix
+++ modules/system/mySys/general.nix
@@ -58,7 +58,7 @@ in
       clean = {
         enable = true;
         dates = "weekly";
-        extraArgs = "--keep 10";
+        extraArgs = "--keep 5";
       };
       flake = "/home/bas/git/nix-conf";
     };
